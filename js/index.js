f85b363573c794c8495b76a1ef04506b875bda32
Buligin98 <buligindaniil98@gmail.com>
2022-11-01 20:06:59 +0400

    Homework 10.1

--- js/index.js
+++ js/index.js
@@ -7,16 +7,13 @@ let off = () => {
 };
 
 let universal = () => {
-  // console.log(document.getElementById("light").src);
-
   let image = document.getElementById("light").src;
-  img = "file:///C:/Users/ads/Desktop/Daniil%20Buligini%20Homework/Homework%2010.1/img/off.png";
+  img =
+    "file:///C:/Users/ads/Desktop/Daniil%20Buligini%20Homework/Homework%2010.1/img/off.png";
 
   if (image == img) {
-    // document.getElementById("light").src = "img/on.png";
     on();
   } else {
-    // document.getElementById("light").src = "img/off.png";
     off();
   }
 };
