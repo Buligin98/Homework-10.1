993804dad74d461768c83cc2ff77d158c2cb6508
Buligin98 <buligindaniil98@gmail.com>
2022-11-01 20:55:02 +0400

    Homework 10.1

--- js/index.js
+++ js/index.js
@@ -6,14 +6,4 @@ let off = () => {
   document.getElementById("light").src = "img/off.png";
 };
 
-let universal = () => {
-  let image = document.getElementById("light").src;
-  img =
-    "file:///C:/Users/ads/Desktop/Daniil%20Buligini%20Homework/Homework%2010.1/img/off.png";
 
-  if (image == img) {
-    on();
-  } else {
-    off();
-  }
-};
