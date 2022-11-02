80a6d8be5230e6ef69e353a946a471809395e1e3
Buligin98 <buligindaniil98@gmail.com>
2022-11-02 21:24:46 +0400

    Homework 10.1

--- js/lamp.js
+++ js/lamp.js
@@ -1,12 +1,12 @@
-let universal = () => {
-    let img = document.getElementById("light").src
-  
-    if (
-      img ==
-      "file:///C:/Users/ads/Desktop/Daniil%20Buligini%20Homework/Homework%2010.1/img/off.png"
-    ) {
-      document.getElementById("light").src = "img/On.png";
-    } else {
-      document.getElementById("light").src = "img/off.png";
-    }
-  };
+const universal = () => {
+  let img = document.getElementById("light").src;
+
+  if (
+    img ==
+    "file:///C:/Users/ads/Desktop/Daniil%20Buligini%20Homework/Homework%2010.1/img/off.png"
+  ) {
+    document.getElementById("light").src = "img/On.png";
+  } else {
+    document.getElementById("light").src = "img/off.png";
+  }
+};
