5337432827455023e64739e692fa9886701070e7
Buligin98 <buligindaniil98@gmail.com>
2022-11-03 16:31:20 +0400

    Homework 10.1

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 let on = () => {
   document.getElementById("light").src = "img/On.png";
+  document.getElementById("universal").innerHTML = "Off";
+  document.getElementById("universal").style.backgroundColor = "#ff0000";
 };
 
 let off = () => {
   document.getElementById("light").src = "img/off.png";
+  document.getElementById("universal").innerHTML = "On";
+  document.getElementById("universal").style.backgroundColor = "#3ff707";
 };
-
-
--- js/lamp.js
+++ js/lamp.js
@@ -6,7 +6,11 @@ const universal = () => {
     "file:///C:/Users/ads/Desktop/Daniil%20Buligini%20Homework/Homework%2010.1/img/off.png"
   ) {
     document.getElementById("light").src = "img/On.png";
+    document.getElementById("universal").innerHTML = "Off";
+    document.getElementById("universal").style.backgroundColor = "#ff0000";
   } else {
     document.getElementById("light").src = "img/off.png";
+    document.getElementById("universal").innerHTML = "On";
+    document.getElementById("universal").style.backgroundColor = "#3ff707";
   }
 };
